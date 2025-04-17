e3ece0ff0ac0156ce20cd2915b40e9a2b32934be
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-04-17 10:22:39 +0300

    Add files via upload

---
+++ ConsoleApp5/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class DerivedClass:Iinterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод, реализованный в классе DerivedClass");
+        }
+
+    }
+}
