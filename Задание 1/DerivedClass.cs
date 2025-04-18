6a1c43e2d4878f45105aea354db6798e9ac51bb6
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-04-18 10:27:42 +0300

    Add files via upload

---
+++ Задание 1/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class DerivedClass:IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод, реализованный в классе DerivedClass");
+        }
+    }
+    
+}
---
+++ Задание 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface 
+{
+//    Задание 1. Реализовать один интерфейс с одним методом в производном классе.
+
+//1.Создайте интерфейс с именем IInterface.
+
+//2.Создайте в интерфейсе метод Method().
+
+//3.Создайте производный класс DerivedClass, который реализует интерфейс IInterface.
+
+//4.Реализуйте метод Method() в классе DerivedClass, чтобы в консольном окне выводился текст:
+
+//Метод, реализованный в классе DerivedClass
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass(); 
+            derivedClass.Method();
+                Console.ReadKey();
+        }
+    }
+}
